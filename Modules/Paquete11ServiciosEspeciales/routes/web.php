ab9f45c0fdf8e577f5734ec1efaf1ae481119037
YeisonCL <yeisonchoqueleon@gmail.com>
2026-07-06 17:23:54 -0400

    Eliminar por completo la estructura del Paquete11ServiciosEspeciales

--- Modules/Paquete11ServiciosEspeciales/routes/web.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-use Illuminate\Support\Facades\Route;
-use Modules\Paquete11ServiciosEspeciales\Http\Controllers\Paquete11ServiciosEspecialesController;
-
-Route::middleware(['auth', 'verified'])->group(function () {
-    Route::resource('paquete11serviciosespeciales', Paquete11ServiciosEspecialesController::class)->names('paquete11serviciosespeciales');
-});
