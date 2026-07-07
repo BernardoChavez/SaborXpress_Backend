9e26f425b57cfb24433715ef5e420f54c6825457
BernardoChavez <bernardochavez595@gmail.com>
2026-07-06 21:35:16 -0400

    Configurar timeout de 3 segundos en SMTP para evitar congelamiento web si el firewall bloquea

--- config/mail.php
+++ config/mail.php
@@ -45,7 +45,7 @@ return [
             'port' => env('MAIL_PORT', 2525),
             'username' => env('MAIL_USERNAME'),
             'password' => env('MAIL_PASSWORD'),
-            'timeout' => null,
+            'timeout' => 3, // Evita que la web se congele por 60 segundos si el puerto está bloqueado por firewall
             'local_domain' => env('MAIL_EHLO_DOMAIN', parse_url((string) env('APP_URL', 'http://localhost'), PHP_URL_HOST)),
         ],
 
